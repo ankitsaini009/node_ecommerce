60efe539ff4c325900ee0d03cefe38289578ec49
Ankit Saini <ankitsaini952005@gmail.com>
2026-02-25 16:38:29 +0530

    react changes 25-2-2026

--- routes/frontend.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const frontend_controller = require("../controllers/frontendController/frontend_controller");
-
-// Add your frontend routes here
-
-router.get("/", frontend_controller.home);
-
-module.exports = router;
